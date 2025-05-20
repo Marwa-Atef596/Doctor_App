498407f630d73eef6cb73918e460af93f2dc35b5
MarwaAtef <marwaatef2001@gmail.com>
2025-05-20 22:01:57 +0300

    screen routes

---
+++ lib/features/doc_app.dart
@@ -0,0 +1,29 @@
+import 'package:doctor_app/core/routing/app_router.dart';
+import 'package:doctor_app/core/routing/routes.dart';
+import 'package:doctor_app/core/theming/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class DocApp extends StatelessWidget {
+  const DocApp({super.key, required this.appRouter});
+  final AppRouter appRouter;
+  @override
+  Widget build(BuildContext context) {
+    return  ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      
+      child: MaterialApp(
+        theme: ThemeData(
+          primaryColor: AppColors.mainColor,
+          scaffoldBackgroundColor: Colors.white,
+          
+        ),
+        debugShowCheckedModeBanner: false,
+        initialRoute: Routes.onBoardingScreen,
+        onGenerateRoute: appRouter.generateRoute,
+      ),
+    );
+  }
+}
