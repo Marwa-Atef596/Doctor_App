129a245545ce00b2120dbfb514f281329c808036
Marwa Atef <marwaatef2001@gmail.com>
2025-06-26 18:51:04 +0300

    .

---
+++ lib/features/login/logic/cubit/login_cubit.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: depend_on_referenced_packages
+
+import 'package:bloc/bloc.dart';
+import 'package:doctor_app/features/login/data/model/login_request_body.dart';
+import 'package:doctor_app/features/login/data/repos/login_repo.dart';
+import 'package:doctor_app/features/login/logic/cubit/login_state.dart';
+import 'package:flutter/material.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  final LoginRepo _loginRepo;
+  LoginCubit(this._loginRepo) : super(LoginState.initial());
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  void emitLoginState(LoginRequestBody loginRequestBody) async {
+    emit(LoginState.loading());
+    final response = await _loginRepo.login(loginRequestBody);
+    response.when(success: (loginResponse) {
+      emit(LoginState.success(loginResponse));
+    }, failure: (error) {
+      emit(LoginState.error(error: error.apiErrorModel.message ?? ''));
+    });
+  }
+}
---
+++ lib/features/login/ui/login_screen.dart
@@ -0,0 +1,77 @@
+import 'package:doctor_app/core/helper/spacing.dart';
+import 'package:doctor_app/core/theming/styles.dart';
+import 'package:doctor_app/core/widgets/app_text_button.dart';
+import 'package:doctor_app/features/login/ui/widgets/already_have_account_text.dart';
+import 'package:doctor_app/features/login/ui/widgets/email_and_password.dart';
+import 'package:doctor_app/features/login/ui/widgets/login_bloc_listener.dart';
+import 'package:doctor_app/features/login/ui/widgets/terms_and_conditions_text_.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../data/model/login_request_body.dart';
+import '../logic/cubit/login_cubit.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 30.w, vertical: 30.h),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Welcome Back',
+                style: Styles.font24BlueBold,
+              ),
+              verticalSpace(8),
+              Text(
+                'We\'re excited to have you back, can\'t wait to see what you\'ve been up to since you last logged in.',
+                style: Styles.font14GreyRegular,
+              ),
+              verticalSpace(36),
+              EmailAndPassword(),
+              verticalSpace(24),
+              Align(
+                alignment: AlignmentDirectional.centerEnd,
+                child: Text(
+                  'Forgot Password?',
+                  style: Styles.font13BlueRegular,
+                ),
+              ),
+              verticalSpace(40),
+              AppTextButton(
+                buttonText: 'Login',
+                textStyle: Styles.font16WhiteMedium,
+                onPressed: () {
+                  validateThenDoLogin(context);
+                },
+              ),
+              verticalSpace(16),
+              TermsAndConditionsText(),
+              verticalSpace(60),
+              AlreadyHaveAccountText(),
+              LoginBlocListener(),
+            ],
+          ),
+        ),
+      )),
+    );
+  }
+
+  void validateThenDoLogin(BuildContext context) {
+    if (context.read<LoginCubit>().formKey.currentState!.validate()) {
+      context.read<LoginCubit>().emitLoginState(
+            LoginRequestBody(
+              email: context.read<LoginCubit>().emailController.text,
+              password: context.read<LoginCubit>().passwordController.text,
+            ),
+          );
+    }
+  }
+}
